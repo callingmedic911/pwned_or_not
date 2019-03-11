b3dd0889773439e90f8d03e57bc06a38dce450d5
Aditya Pandey <aditya@autospace.co>
2019-03-11 13:42:17 +0530

    Resolve missing conflict

--- lib/breach.dart
+++ lib/breach.dart
@@ -7,7 +7,6 @@ class Breach {
   final bool isSensitive;
   final bool isRetired;
   final bool isSpamList;
-<<<<<<< HEAD
   final String logoPath;
   final List<String> dataClasses;
   final String breachDate;
@@ -24,15 +23,6 @@ class Breach {
       this.logoPath,
       this.dataClasses,
       this.breachDate});
-=======
-  final String logoType;
-  final List<String> dataClasses;
-  final String breachDate;
-
-  Breach({this.name, this.title, this.description, this.isVerified,
-    this.isFabricated, this.isSensitive, this.isRetired, this.isSpamList,
-    this.logoType, this.dataClasses, this.breachDate});
->>>>>>> 84e1d376e86c7930777a9c29757763cc8e81dafe
 
   factory Breach.fromJson(Map<String, dynamic> json) {
     return Breach(
@@ -44,17 +34,9 @@ class Breach {
       isSensitive: json['IsSensitive'],
       isRetired: json['IsRetired'],
       isSpamList: json['IsSpamList'],
-<<<<<<< HEAD
       logoPath: json['LogoPath'],
-=======
-      logoType: json['LogoType'],
->>>>>>> 84e1d376e86c7930777a9c29757763cc8e81dafe
       dataClasses: List.from(json["DataClasses"]),
       breachDate: json["BreachDate"],
     );
   }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 84e1d376e86c7930777a9c29757763cc8e81dafe
